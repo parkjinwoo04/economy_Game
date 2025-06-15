4fcef5dc931c5cffaf9899c86100efc3abbd6dc2
parkjinwoo04 <133664212+parkjinwoo04@users.noreply.github.com>
2025-06-15 11:40:41 +0900

    Create login.js

---
+++ login.js
@@ -0,0 +1,39 @@
+const login_Id = document.getElementById("id").value;
+const login_Pw = document.getElementById("pw").value;
+const login_rPw = document.getElementById("r_pw").value;
+
+
+
+function login() {
+  const saved_Pw = localStorage.hetItem(login_Id);
+  
+  if (login_Id === "" || login_Pw === "") {
+    alert ("아이디와 비밀번호를 입력해주세요");
+  }
+  if (saved_Pw === null) {
+    alert ("존재하지 않는 아이디입니다. 회원가입을 먼저 해주세요");
+  } else if (saved_Pw !== login_Pw) {
+    alert ("비밀번호가 일치하지 않습니다.");
+  } else {
+    alert ("로그인 성공");
+    localStorage.setItem("loggedInuser", id);
+  }
+}
+
+
+function create_user {
+  if (login_Id === "" || login_Pw === "") {
+    alert ("아이디와 비밀번호를 입력해주세요");
+    return;
+  } else if (login_Pw !== login_rPw) {
+    alert ("비밀번호가 일치하지 않습니다.");
+    return;
+  }
+  if (localStorage.getItem(login_Id)) {
+    alert ("이미 존재하는 아이디입니다.");
+    return;
+  }
+  localStorage.setItem(login_Id, login_Pw);
+  alert ("회원가입이 완료되었습니다.");
+  location,href = "index.html";
+}
